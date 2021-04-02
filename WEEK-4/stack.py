148782da79feb7f87b16edcf0f60d589d8b5e85f
Jean <mizero2@gmail.com>
2021-04-01 20:20:56 -0600

    Add stack file

---
+++ WEEK-4/stack.py
@@ -0,0 +1,39 @@
+"""The stack abstract data type is defined by the following structure and operations.
+A stack is structured, as described above,
+as an ordered collection of items where items are added to and removed from the end called the “top.”
+Stacks are ordered LIFO. """
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
